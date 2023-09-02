e2bb46b3dffa5be8f18a02bb17404aa6ab18a08c
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-09-02 13:45:03 +0900

    詳細の表示/非表示切り替え機能 (#8)
    * isShowDetailを追加
    
    * mutableStateに変更

--- app/src/main/java/com/example/jetpackprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetpackprofile/MainActivity.kt
@@ -4,28 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackprofile.components.CompanySection
 import com.example.jetpackprofile.components.DetailSection
-import com.example.jetpackprofile.components.Label
 import com.example.jetpackprofile.ui.theme.JetpackProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -80,15 +72,18 @@ fun MainContent() {
     Spacer(modifier = Modifier.height(20.dp))
 
     // 詳細表示ボタン
+    var isShowDetail by remember { mutableStateOf(false) }
     Button(
       modifier = Modifier.fillMaxWidth(),
       colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF85F6A)),
-      onClick = { /*TODO*/ }) {
+      onClick = { isShowDetail = !isShowDetail }) {
       Text(text = "詳細を表示", color = Color.White)
     }
     Spacer(modifier = Modifier.height(20.dp))
 
     // 趣味と居住地セクション
-    DetailSection()
+    if (isShowDetail) {
+      DetailSection()
+    }
   }
 }
