8c71d00dfa7c7bc09966f1921f7e5f71eb6a3d7b
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-09-02 13:43:41 +0900

    mutableStateに変更

--- app/src/main/java/com/example/jetpackprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetpackprofile/MainActivity.kt
@@ -4,28 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackprofile.components.CompanySection
 import com.example.jetpackprofile.components.DetailSection
-import com.example.jetpackprofile.components.Label
 import com.example.jetpackprofile.ui.theme.JetpackProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -80,7 +72,7 @@ fun MainContent() {
     Spacer(modifier = Modifier.height(20.dp))
 
     // 詳細表示ボタン
-    var isShowDetail = false
+    var isShowDetail by remember { mutableStateOf(false) }
     Button(
       modifier = Modifier.fillMaxWidth(),
       colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF85F6A)),
